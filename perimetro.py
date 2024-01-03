41e1c869fc6db6cdd8080ebf18ee814627b09cc1
Gustavo Muzzi Gomes <152799550+gustavomuzzigomes@users.noreply.github.com>
2024-01-03 11:16:46 -0300

    perimetro.py

--- perimetro.py
+++ perimetro.py
@@ -16,17 +16,20 @@ while True:
             lado2 = float(input("Digite o tamanho do segundo lado do triângulo: "))
             lado3 = float(input("Digite o tamanho do terceiro lado do triângulo: "))
             perimetro = lado1 + lado2 + lado3 
+            figura = 'triangulo'
             print("\nO valor do perímetro do %s é %.1f" %(figura,perimetro))
 
         elif figura == '2':
             lado = float(input("\nDigite o tamanho do lado do quadrado: "))
             perimetro = 4*lado
+            figura = 'quadrado'
             print("\nO valor do perímetro do %s é %.1f" %(figura,perimetro))
 
         elif figura == '3':
             base = float(input("\nDigite o tamanho da base do retângulo: "))
             altura = float(input("Digite o tamanho da altura do retângulo: "))
             perimetro = 2*base + 2*altura
+            figura = 'retângulo'
             print("\nO valor do perímetro do %s é %.1f" %(figura,perimetro))
 
         elif figura == '4':
@@ -35,17 +38,20 @@ while True:
             lado1 = float(input("Digite o tamanho de um lado do trapézio: "))
             lado2 = float(input("Digite o tamanho do outro lado do trapézio: "))
             perimetro = base_maior + base_menor + lado1 + lado2
+            figura = 'trapézio'
             print("\nO valor do perímetro do %s é %.1f" %(figura,perimetro))
 
         elif figura == '5':
             lado1 = float(input("\nDigite o tamanho de um lado do losango:  "))
             lado2 = float(input("Digite o tamanho do outro lado do losango: "))
             perimetro = 2*lado1 + 2*lado2
+            figura ='losango'
             print("\nO valor do perímetro do %s é %.1f" %(figura,perimetro))
 
         elif figura == '6':
             raio = float(input("\nDigite o tamanho do raio do círculo: "))
             perimetro = 2*3.14*raio
+            figura = 'círculo'
             print("\nO valor do perímetro do %s é %.1f" %(figura,perimetro))
 
         else:
